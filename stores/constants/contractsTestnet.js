a57d7d369dea8dbbf2c2981589d98a919e290245
a.maik <poseidonisanog@gmail.com>
2023-01-17 00:07:56 +0100

    Update contractsTestnet.js

--- stores/constants/contractsTestnet.js
+++ stores/constants/contractsTestnet.js
@@ -1,6 +1,6 @@
 import abis from "../abis";
 
-export const GOV_TOKEN_ADDRESS = "0x212e541f3cF13AC58fE504ce10c0db4AF1eC94ae";
+export const GOV_TOKEN_ADDRESS = "0x389Aa61b7f086bF64FBD6d06b5e7b37B5f169484";
 export const GOV_TOKEN_NAME = "Vara";
 export const GOV_TOKEN_SYMBOL = "VARA";
 export const GOV_TOKEN_DECIMALS = 18;
@@ -8,7 +8,7 @@ export const GOV_TOKEN_LOGO = "https://github.com/equilibre-finance.png";
 export const GOV_TOKEN_ABI = abis.tokenABI;
 
 //VotingEscrow
-export const VE_TOKEN_ADDRESS = "0x09af51b5D5d4D61b64C64Aaf8Ba0B1133E55fe06";
+export const VE_TOKEN_ADDRESS = "0x7606445E886585f699FE6dA09Dfd6e8f70436953";
 export const VE_TOKEN_NAME = "veVARA";
 export const VE_TOKEN_SYMBOL = "veVARA";
 export const VE_TOKEN_DECIMALS = 18;
@@ -31,22 +31,22 @@ export const KAVA_LOGO =
   "https://assets.coingecko.com/coins/images/9761/large/kava.png";
 
 // PairFactory
-export const FACTORY_ADDRESS = "0x7e6F3eC9F81051e8fb34B14CA76C1fEd39237e47";
+export const FACTORY_ADDRESS = "0xbB94C08E163Db531C56486249549007878E26176";
 export const FACTORY_ABI = abis.factoryABI;
 // Classic Router
-export const ROUTER_ADDRESS = "0x84C645B1d7499cD9f9617a271904C3d6A7214836";
+export const ROUTER_ADDRESS = "0x6E7bA88B5b2F76735a15c817B1F9E1faC51c01c4";
 export const ROUTER_ABI = abis.routerABI;
 //RewardsDistributor
-export const VE_DIST_ADDRESS = "0x02DC3F98A324658Eb6978b7231Fb9980C2E8D5e6";
+export const VE_DIST_ADDRESS = "0x6DA0e51424610C8b4aFE530021d0A0a399381Ed9";
 export const VE_DIST_ABI = abis.veDistABI;
 
-export const VOTER_ADDRESS = "0xC0e936D14c5a815642B1b7f342486716E3d6CE9e";
+export const VOTER_ADDRESS = "0x5CDe011200F6D1aF805bb4A1a8e8B2eb19B0A59C";
 export const VOTER_ABI = abis.voterABI;
 
-export const LIBRARY_ADDRESS = "0x2428624C85109F53ad409a5D959fE73Ae3562Dba";
+export const LIBRARY_ADDRESS = "0xf6956F806bAbeb9E1Fd0088eCcdA0342d02FC43C";
 export const LIBRARY_ABI = abis.solidlyLibraryABI;
 
-export const WRAPPED_EXTERNAL_BRIBE_FACTORY_ADDRESS = "0xBf4d0BC6A6E1f356151D8258a2f801990A3EF307";
+export const WRAPPED_EXTERNAL_BRIBE_FACTORY_ADDRESS = "0xb53a0E0198b147448A336B0Cf78258842B876b35";
 
 export const ERC20_ABI = abis.erc20ABI;
 export const PAIR_ABI = abis.pairABI;
